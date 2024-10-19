8dd456a0a7b2a98c858c0163522d70055b3bea29
Max Sajedi <sajedi@usc.edu>
2024-10-19 09:02:39 -0400

    upload my hand-written version of graph.h

---
+++ common/Personal/graph.h
@@ -0,0 +1,48 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+using Vertex = int;
+
+struct graph
+{
+	int num_edges;
+	int num_nodes;
+	
+	int* outgoing_starts;
+	Vertex* outgoing_edges;
+
+	int* incoming_starts;
+	Vertex* incoming_edges;
+	
+};
+
+using Graph = graph*;
+
+/*Getters */
+static inline int num_nodes(const Graph);
+static inline int num_edges(const Graph);
+
+static inline const Vertex* outgoing_begin(const Graph, Vertex);
+static inline const Vertex* outgoing_end(const Graph, Vertex);
+static inline int outgoing_size(const Graph, Vertex);
+
+static inline const Vertex* incoming_begin(const Graph, Vertex);
+static inline const Vertex* incoming_end(const Graph, Vertex);
+static inline int incoming_size(const Graph, Vertex);
+
+
+/* IO */
+Graph load_graph(const char* filename);
+Graph load_graph_binary(const char* filename);
+void store_graph_binary(const char* filename, Graph);
+
+void print_graph(const graph*);
+
+
+/* Deallocation*/
+void free_graph(Graph);
+
+/* Included here to enable inlining. Don't look. */
+#include "graph_internal.h"
+
+#endif
