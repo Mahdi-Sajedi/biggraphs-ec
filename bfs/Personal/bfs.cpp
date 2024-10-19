ebef99449dc6dce11efe2fb6a6e5902eac354d60
Max Sajedi <sajedi@usc.edu>
2024-10-19 11:25:13 -0400

    Update bfs.cpp

--- bfs/Personal/bfs.cpp
+++ bfs/Personal/bfs.cpp
@@ -1,1 +1,99 @@
+#include "bfs.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstddef>
+#include <omp.h>
+
+#include "../common/CycleTimer.h"
+#include "../common/graph.h"
+
+#define ROOT_NODE_ID 0
+#define NOT_VISITED_MARKER -1
+
+void vertex_set_clear(vertex_set* list) {
+    list->count = 0;
+}
+
+void vertex_set_init(vertex_set* list, int count) {
+    list->max_vertices = count;
+    list->vertices = (int*)malloc(sizeof(int) * list->max_vertices);
+    vertex_set_clear(list);
+}
+
+// Take one step of "top-down" BFS. For each vertex on the frontier,
+// follow ll outgoing edges, and add all neighboring vertices to the 
+// new_frontier
+void top_down_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    int* distances)
+{
+    for (int i=0; i<frontier->count; i++) {
+
+        int node = frontier->vertices[i];
+
+        int start_edge = g->outgoing_starts[node];
+        int end_edge = (node == g->num_nodes -1) ? g->num_edges : g->outgoing_starts[node+1];
+
+        // attempt to add all neighbors to the new frontier   
+        for (int neighbor=start_edge; neighbor<end_edge; neighbor++) {
+            int outgoing = g->outgoing_edges[neighbor];
+
+            if (distances[outgoing] == NOT_VISITED_MARKER) {
+                distances[outgoing] = distances[node] + 1;
+                new_frontier->vertices[new_frontier->count++] = outgoing;
+            }
+        }
+    }
+}
+
+// Implements top-down BFS.
+//
+// Result of execution: for each node in the graph, the distance to the root is stored in sol.distances
+void bfs_top_down(Graph graph, solution* sol) {
+
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+
+    vertex_set* frontier = &list1;
+    vertex_set* new_frontier = &list2;
+
+    // initialize all nodes to NOT_VISITED
+    for (int i=0; i<graph-num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (frontier->count != 0) {
+        
+#ifdef VERBOSE
+        double start_time = CycleTimer::currentSeconds();
+#endif
+
+        vertex_set_clear(new_frontier);
+        top_down_step(graph, frontier, new_frontier, sol->distances);
+
+#ifdef VERBOSE
+        double end_time = CycleTimer::currentSeconds();
+        printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+#endif
+
+        // swap pointers
+        vertex_set* tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+    }
+}
+
+void bfs_bottom_up(Graph graph, solution* sol)
+{
+    // refer to handout for details
+    // you can implement bottom_up_step() if see fit
+}
